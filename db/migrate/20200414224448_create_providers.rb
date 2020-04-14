af3c93c341b1983d3383e15b8861f2335512326a
bbennett7 <bryn.bennett@gmail.com>
2020-04-14 15:57:01 -0700

    Create providers migration

---
+++ db/migrate/20200414224448_create_providers.rb
@@ -0,0 +1,16 @@
+class CreateProviders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :providers do |t|
+      t.string :provider_name
+      t.string :street_address
+      t.string :city
+      t.string :state 
+      t.string :zip 
+      t.string :county
+      t.string :country
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
